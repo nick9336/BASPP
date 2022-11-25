0241a9db52990fcb46ddbaab9092104ca2df8c46
nick9336 <112458559+nick9336@users.noreply.github.com>
2022-11-26 02:11:43 +0800

    Add files via upload

---
+++ sell.php
@@ -0,0 +1,108 @@
+<?php
+
+include 'config.php';
+
+session_start();
+
+$user_id = $_SESSION['user_id'];
+
+if(!isset($user_id)){
+   header('location:login.php');
+};
+
+if(isset($_POST['add_product'])){
+
+   $name = mysqli_real_escape_string($conn, $_POST['name']);
+   $price = $_POST['price'];
+   $des = $_POST['des'];
+   $image = $_FILES['image']['name'];
+   $image_size = $_FILES['image']['size'];
+   $image_tmp_name = $_FILES['image']['tmp_name'];
+   $image_folder = 'uploaded_img/'.$image;
+
+   $select_product_name = mysqli_query($conn, "SELECT name FROM `products` WHERE name = '$name'") or die('query failed');
+
+   if(mysqli_num_rows($select_product_name) > 0){
+      $message[] = 'product name already added';
+   }else{
+      $add_product_query = mysqli_query($conn, "INSERT INTO `products`(name, price, des, image) VALUES('$name', '$price', '$des', '$image')") or die('query failed');
+
+      if($add_product_query){
+         if($image_size > 2000000){
+            $message[] = 'image size is too large';
+         }else{
+            move_uploaded_file($image_tmp_name, $image_folder);
+            $message[] = 'product added successfully!';
+         }
+      }else{
+         $message[] = 'product could not be added!';
+      }
+   }
+}
+
+if(isset($_GET['delete'])){
+   $delete_id = $_GET['delete'];
+   $delete_image_query = mysqli_query($conn, "SELECT image FROM `products` WHERE id = '$delete_id'") or die('query failed');
+   $fetch_delete_image = mysqli_fetch_assoc($delete_image_query);
+   unlink('uploaded_img/'.$fetch_delete_image['image']);
+   mysqli_query($conn, "DELETE FROM `products` WHERE id = '$delete_id'") or die('query failed');
+   header('location:admin_products.php');
+}
+
+
+?>
+
+<!DOCTYPE html>
+<html lang="en">
+<head>
+   <meta charset="UTF-8">
+   <meta http-equiv="X-UA-Compatible" content="IE=edge">
+   <meta name="viewport" content="width=device-width, initial-scale=1.0">
+   <title>sell</title>
+
+   <!-- font awesome cdn link  -->
+   <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.0.0/css/all.min.css">
+
+   <!-- custom admin css file link  -->
+   <link rel="stylesheet" href="style.css">
+   <link rel="stylesheet" href="BASPP_style.css">
+
+</head>
+<body>
+   
+<?php include 'header.php'; ?>
+
+<!-- product CRUD section starts  -->
+
+<section class="add-products">
+
+   <h1 class="title">sell products</h1>
+
+   <form action="" method="post" enctype="multipart/form-data">
+      <h3>add product</h3>
+      <input type="text" name="name" class="box" placeholder="enter product name" required>
+      <input type="number" min="0" name="price" class="box" placeholder="enter product price" required>
+	  <input type="text" name="des" class="box" placeholder="enter description" required>
+      <input type="file" name="image" accept="image/jpg, image/jpeg, image/png" class="box" required>
+      <input type="submit" value="add product" name="add_product" class="btn">
+   </form>
+
+</section>
+
+<!-- product CRUD section ends -->
+
+
+
+<?php include 'footer.php'; ?>
+
+
+
+
+
+
+
+<!-- custom admin js file link  -->
+<script src="admin_script.js"></script>
+
+</body>
+</html>
